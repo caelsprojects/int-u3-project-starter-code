49bda02994c9d3bdb7b5fbaf870939050b7ecf2f
CaelsProjects <caelbarnesschool@gmail.com>
2025-06-13 13:16:32 -0400

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Declare variables below to save the different divs of your story.
 
+let openingStory = document.querySelector(".story-opening")
+
+let optionOneButton = document.querySelector(".option-one")
+let optionTwoButton = document.querySelector(".option-two")
+
+let optionOneScreen = document.querySelector(".option-one-screen")
+let optionTwoScreen = document.querySelector(".option-two-screen")
+
+let optionOneEnd = document.querySelector(".option-one-end")
+let optionTwoEnd = document.querySelector(".option-two-end")
 
 
+
+let title = document.querySelector('.title')
 
 
 
@@ -11,14 +23,30 @@
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
+title.addEventListener('click', function() {
+    openingStory.style.display = "block";
+    optionOneScreen.style.display = "none";
+optionOneEnd.style.display = "none";
+optionTwoEnd.style.display = "none";
+optionTwoScreen.style.display = "none";
+});
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+optionOneButton.addEventListener('click', function() {
+openingStory.style.display = "none";
+optionOneScreen.style.display = "block";
+optionOneEnd.style.display = "block";
+optionTwoEnd.style.display = "none";
+optionTwoScreen.style.display = "none";
 
-// });
+});
 
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
+optionTwoButton.addEventListener('click', function(){
+openingStory.style.display = "none";
+optionTwoEnd.style.display = "block";
+optionTwoScreen.style.display = "block";
+optionOneScreen.style.display = "none";
+optionOneEnd.style.display = "none";
+ });
 
 
 // INSERT_VARIABLE.addEventListener('click', function(){
